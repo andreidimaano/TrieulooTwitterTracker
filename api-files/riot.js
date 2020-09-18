7ee826510350672a856bd34cd5a635e101efa4ec
Andrei <andreiidimaano@gmail.com>
2020-09-18 16:22:43 -0700

    add riotapi

--- api-files/riot.js
+++ api-files/riot.js
@@ -1,25 +1,58 @@
-const dotenv = require('dotenv');
-const axios = require('axios').default;
-
-axios.defaults.baseURL = 'https://na1.api.riotgames.com/lol/';
+const axios = require("./axios");
 
 getAccount("Trieuloo");
 
+var gameId;
 
 function getAccount(summonerName){
     
-    axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`,)
-    const url = `?api_key=${process.env.API_KEY}`;
-    
-    https.get(url, (res) => {
-        // console.log(res.statusCode);
+    axios.get(`/summoner/v4/summoners/by-name/${summonerName}`)
+    .then((response) => {
+        const accountId = response.data.accountId;
+        // console.log(response.data.accountId);
+        getMatch(accountId, 1);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 
-        res.on('data', (d) => {
-            const accountData = JSON.parse(d);
-            console.log("accountData: ", JSON.parse(d));
-            const accountId = accountData.accountId;
-            const amountOfGames = 1;
-            // getMatch(accountId, amountOfGames);
-        });
-    });
 }
+
+function getMatch(accountId,amountOfGames) {
+    axios.get(`/match/v4/matchlists/by-account/${accountId}`,{
+        params: {
+            endIndex: amountOfGames,
+            beginIndex: 0
+        }
+    })
+    .then((response) => {
+        const match = response.data.matches[0];
+        gameId = match.gameId;
+        const champId = match.champion;
+        // console.log(response.data);
+        getMatchData(gameId, champId);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+};
+
+function getMatchData(gameId, champId) {
+
+    axios.get(`/match/v4/matches/${gameId}`)
+    .then((response) => {
+        const body = response.data;
+
+        const participant = body.participants.filter(participant => participant.championId === champId);
+
+        if(participant[0].stats.win == true){
+            console.log("Okay good job, you won but when will you hit challenger lol.");
+        } else {
+            console.log("XDDDDDDDDD. What else could I have expected.")
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+};
