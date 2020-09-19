f75add73c8d47984818e61a188a792463f3e43ba
alfredzhuang <alfredzhuang123@gmail.com>
2020-09-19 12:14:58 -0700

    added nonrepeating tweet functionality

--- twitterbot.js
+++ twitterbot.js
@@ -1,17 +1,28 @@
 var Twit = require('twit');
 require('dotenv').config();
+const Datastore = require('nedb');
 
 const { getMessages } = require("./messages.js");
+
+const database = new Datastore('matches.db');
+database.loadDatabase();
 
 async function messages() {
     let messages = await getMessages();
     let matchId, funnyText, funnyStat;
     [matchId, funnyText, funnyStat] = [messages[0], messages[1], messages[2]];
-    tweetIt(matchId, funnyText, funnyStat);
+    database.find({matchId}, function(err, docs) {
+        if(docs === undefined || docs.length=== 0) {
+            database.insert({matchId});
+            tweetIt(matchId, funnyText, funnyStat);
+        }
+        else {
+            console.log("No new tweet");
+        }
+    });
 }
 
-    messages();
-
+messages();
 
 function tweetIt(matchId, funnyText, funnyStat) {
     var T = new Twit({
@@ -22,18 +33,20 @@ function tweetIt(matchId, funnyText, funnyStat) {
     });
 
     var tweet = {
-        status: funnyText + "\n" + funnyStat[2][0] + ": " + funnyStat[2][1] + "\n" +  funnyStat[3][0] + ": " + funnyStat[3][1] + "\n" + funnyStat[4][0] + ": " + funnyStat[4][1] + "\n"
-                + funnyStat[5][0] + ": " + funnyStat[5][1] + "\n" + funnyStat[6][0] + ": " + funnyStat[6][1] + "\n" + funnyStat[7][0] + ": " + funnyStat[7][1]
+        status: funnyText + "\n" + "\n" + funnyStat[2][0] + ": " + funnyStat[2][1] 
+                + "\n" +  funnyStat[3][0] + ": " + funnyStat[3][1] 
+                + "\n" + funnyStat[4][0] + ": " + funnyStat[4][1] 
+                + "\n"+ funnyStat[5][0] + ": " + funnyStat[5][1] 
+                + "\n" + funnyStat[6][0] + ": " + funnyStat[6][1] 
+                + "\n" + funnyStat[7][0] + ": " + funnyStat[7][1]
     }
 
     T.post('statuses/update', tweet, tweeted);
 
     function tweeted(err, data, response) {
-        if(err) {
-            console.log("Did not go through");
-        }
-        else {
-            console.log("It worked!");
-        }
+        var currentDate = new Date();
+        var timestamp = currentDate.getDate() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getFullYear() + 
+                        " " + currentDate.getHours() + ":" + currentDate.getMinutes();
+        console.log("Tweeted at " + timestamp);
     }
 }
